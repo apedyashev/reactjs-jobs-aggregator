3e727f22d27e7c90c4cb9db22cefb30ec8dc115b
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2016-08-20 21:37:59 +0200

    Setup AVA to suport webpack aliases

---
+++ src/components/dumb/SubscriptionList/index.spec.js
@@ -0,0 +1,6 @@
+import test from 'ava';
+// import SubscriptionList from './index.js';
+//
+test('foo', (t) => {
+  t.pass();
+});
--- src/components/pages/Login/Form.spec.js
+++ src/components/pages/Login/Form.spec.js
@@ -1,11 +1,27 @@
 import test from 'ava';
-// import {shallow, mount} from 'enzyme';
+// import {/* shallow, */mount} from 'enzyme';
 // import React from 'react';
 // import LoginForm from './Form';
 // import {Input} from 'components/dumb/Base';
+// import configureStore from 'redux-mock-store';
+// import jsdom from 'jsdom';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// import muiTheme from 'components/MuiTheme';
 //
-test.skip('The redux form HOC', () => {
-//   const wrapper = shallow(<LoginForm />);
+// const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+// global.document = doc;
+// global.window = doc.defaultView;
 //
-//   t.is(wrapper.contains(Input), true, '');
+// const mockStore = configureStore();
+
+test('The redux form HOC', (t) => {
+  // const store = mockStore({});
+  // const wrapper = mount(
+  //   <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
+  //     <LoginForm store={store} methods={() => {}} />
+  //   </MuiThemeProvider>
+  // );
+  t.pass();
+  // t.is(wrapper.contains(Input), true, '');
 });
